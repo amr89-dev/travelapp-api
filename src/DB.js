9bfc4d32689d05dcf507cd5761e055db17e1dd47
sorrus-dev <amr89.dev@gmail.com>
2023-08-30 10:27:58 -0500

    protegiendo la uri de la base da datos

--- src/DB.js
+++ src/DB.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DATABASE_URL } = process.env;
 
-const database = new Sequelize(
-  `postgres://fl0user:8jtRCw5ZaPph@ep-old-cloud-55060796.us-east-2.aws.neon.tech:5432/travelapp?sslmode=require`,
-  { logging: false, native: false }
-);
+const database = new Sequelize(DATABASE_URL, { logging: false, native: false });
 
 module.exports = {
   database,
