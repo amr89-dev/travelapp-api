b24351b0ab79a1c2a5cf3acdfcec1835342378ef
sorrus-dev <amr89.dev@gmail.com>
2023-08-29 22:24:24 -0500

    modificando url

--- src/DB.js
+++ src/DB.js
@@ -4,7 +4,7 @@ const { Sequelize } = require("sequelize");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const database = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/travelapp`,
+  `postgres://fl0user:8jtRCw5ZaPph@ep-old-cloud-55060796.us-east-2.aws.neon.tech:5432/travelapp?sslmode=require`,
   { logging: false, native: false }
 );
 
