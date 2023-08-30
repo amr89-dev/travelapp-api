7c4c962e36a9a288e6e3f104ba591116daf1e3ac
sorrus-dev <amr89.dev@gmail.com>
2023-08-30 13:26:55 -0500

    fix nodemailer 3

--- src/helper/mailer.js
+++ src/helper/mailer.js
@@ -4,10 +4,9 @@ require("dotenv").config();
 const { NODEMAILER_EMAIL, NODEMAILER_PASSWORD } = process.env;
 
 const transporter = nodemailer.createTransport({
-  service: "hotmail",
   host: "smtp-mail.outlook.com",
   port: 587,
-  secure: false,
+  secure: false, // upgrade later with STARTTLS
   auth: {
     user: NODEMAILER_EMAIL,
     pass: NODEMAILER_PASSWORD,
@@ -17,7 +16,8 @@ transporter.verify(function (error, success) {
   if (error) {
     console.log(error);
   } else {
-    console.log("Server is ready to take our messages");
+    console.log("SMTP is ready");
   }
 });
+
 module.exports = transporter;
